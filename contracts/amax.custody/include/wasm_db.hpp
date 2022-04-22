6f75d1d64688e51e78eac239e3ac7bcb437b1bf4
metamaster2021 <aws4amazon@163.com>
2022-04-22 18:07:05 +0800

    fix table.set() in custody

--- contracts/amax.custody/include/wasm_db.hpp
+++ contracts/amax.custody/include/wasm_db.hpp
@@ -14,7 +14,7 @@ public:
     template<typename Lambda>
     void set(uint64_t pk, eosio::name payer, Lambda&& setter ) {
         auto itr = base::find(pk);
-        if (itr != base::end()) {
+        if (itr == base::end()) {
             base::emplace(payer, setter);
         } else {
             base::modify(itr, payer, setter);
