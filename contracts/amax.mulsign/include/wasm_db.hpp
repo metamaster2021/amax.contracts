fb68d5c585d4a87424570b353d9f0a7f8a758d44
masteray <masteraychen@gmail.com>
2022-05-23 22:51:35 +0800

    fix record set

--- contracts/amax.mulsign/include/wasm_db.hpp
+++ contracts/amax.mulsign/include/wasm_db.hpp
@@ -76,7 +76,7 @@ public:
 
         typename RecordType::idx_t idx(code, scope);
         auto itr = idx.find( record.primary_key() );
-        check( itr == idx.end(), "record not found" );
+        check( itr != idx.end(), "record not found" );
 
         idx.modify( itr, same_payer, [&]( auto& item ) {
             item = record;
