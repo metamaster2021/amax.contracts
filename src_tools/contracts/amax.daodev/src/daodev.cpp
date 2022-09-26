8cf8ae58d041ed0a29181faebcb62b778d141950
masteray <masteraychen@gmail.com>
2022-09-26 10:29:06 +0800

    update

--- src_tools/contracts/amax.daodev/src/daodev.cpp
+++ src_tools/contracts/amax.daodev/src/daodev.cpp
@@ -16,14 +16,16 @@ static constexpr name AMAX_BANK     = name("amax.token");
 static constexpr name CNYD_BANK     = name("cnyd.token");
 
 void daodev::init() {
-    check(false, "not allowed");
+    // check(false, "not allowed");
     require_auth( _self );
+
+    _gstate.admin = "armoniaadmin"_n;
 }
 
 void daodev::ontransfer(const name& from, const name& to, const asset& quant, const string& memo) {
     if(_self == from || to != _self) return;
 
-    check(quant.amount > 0, "must transfer positive quantity");
+    check(quant.amount > 0, "must transfer positive quantity: " + quant.to_string());
 
     if (from == "amax.xchain"_n) {
         auto fund = quant * _gstate.xchain_conf.xchain_fund_pct;
