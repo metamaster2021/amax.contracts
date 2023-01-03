46e83623a0263f5fb2f5e090edc5c0910480ba7a
metamaster2021 <aws4amazon@163.com>
2023-01-03 11:18:46 +0800

    add amax.reward contract

---
+++ src_system/contracts/amax.reward/include/amax.reward/amax.reward.hpp
@@ -0,0 +1,135 @@
+#pragma once
+
+#include <eosio/asset.hpp>
+#include <eosio/eosio.hpp>
+#include <eosio/singleton.hpp>
+
+#include <string>
+
+namespace amax {
+
+   using std::string;
+   using eosio::contract;
+   using eosio::name;
+   using eosio::asset;
+   using eosio::symbol;
+   using eosio::block_timestamp;
+
+   static constexpr name      SYSTEM_CONTRACT = "amax"_n;
+   static constexpr name      CORE_TOKEN = "amax.token"_n;
+   static const     symbol    CORE_SYMBOL     = symbol("AMAX", 8);
+
+   #define CORE_ASSET(amount) asset(amount, CORE_SYMBOL)
+
+   /**
+    * The `amax.reward` contract is used as a reward dispatcher contract for amax.system contract.
+    *
+    */
+   class [[eosio::contract("amax.reward")]] amax_reward : public contract {
+      public:
+
+         amax_reward( name s, name code, eosio::datastream<const char*> ds ):
+               contract(s, code, ds), _global(get_self(), get_self().value)
+         {
+            _gstate  = _global.exists() ? _global.get() : global_state{};
+         }
+
+         ~amax_reward() {
+            _global.set(_gstate, get_self());
+         }
+
+         /**
+          * update votes.
+          *
+          * @param voter_name - the account of voter,
+          * @param producers - producer list
+          * @param old_votes - old votes value,
+          * @param new_votes - new votes value.
+          */
+         [[eosio::action]]
+         void updatevotes(const name& voter_name, const std::set<name>& producers, double votes);
+
+         /**
+          * add reward for producer, deduct from the reward balance
+          *
+          * @param producer_name - the account of producer,
+          * @param quantity - reward quantity
+          */
+         [[eosio::action]]
+         void addrewards(const name& producer_name, const asset& quantity);
+
+
+         /**
+          * claim rewards for voter
+          *
+          * @param voter_name - the account of voter
+          */
+         [[eosio::action]]
+         void claimrewards(const name& voter_name);
+
+        /**
+         * Notify by transfer() of xtoken contract
+         *
+         * @param from - the account to transfer from,
+         * @param to - the account to be transferred to,
+         * @param quantity - the quantity of tokens to be transferred,
+         * @param memo - the memo string to accompany the transaction.
+         */
+        [[eosio::on_notify("amax.token::transfer")]]
+        void ontransfer(   const name &from,
+                           const name &to,
+                           const asset &quantity,
+                           const string &memo);
+
+
+      private:
+
+
+         struct [[eosio::table]] global_state {
+            asset                reward_balance = CORE_ASSET(0);
+            asset                total_rewards  = CORE_ASSET(0);
+
+            typedef eosio::singleton< "global"_n, global_state >   table;
+         };
+
+         struct [[eosio::table]] producer {
+            name              owner;
+            asset             unallocated_rewards  = CORE_ASSET(0);
+            asset             allocating_rewards   = CORE_ASSET(0);
+            asset             allocated_rewards    = CORE_ASSET(0);
+            double            votes                = 0;
+            double            reward_per_vote      = 0;
+            block_timestamp   update_at;
+
+            uint64_t primary_key()const { return owner.value; }
+
+            typedef eosio::multi_index< "producers"_n, producer > table;
+         };
+
+
+         struct [[eosio::table]] vote_reward_info {
+            double               last_reward_per_vote;
+         };
+
+         struct [[eosio::table]] voter {
+            name                             owner;
+            // name                 producer;
+            double                           votes;
+            // double               last_reward_per_vote;
+            std::map<name, vote_reward_info> producers;
+            asset                            unclaimed_rewards;
+            asset                            claimed_rewards;
+            block_timestamp                  update_at;
+
+            uint64_t primary_key()const { return owner.value; }
+
+            typedef eosio::multi_index< "voters"_n, voter > table;
+         };
+
+   private:
+      global_state::table     _global;
+      global_state            _gstate;
+
+   };
+
+}
---
+++ src_system/contracts/amax.reward/src/amax.reward.cpp
@@ -0,0 +1,198 @@
+#include <amax.reward/amax.reward.hpp>
+#include <eosio/system.hpp>
+
+namespace amax {
+
+using namespace eosio;
+
+static constexpr name ACTIVE_PERM       = "active"_n;
+
+struct amax_token {
+      void transfer( const name&    from,
+                     const name&    to,
+                     const asset&   quantity,
+                     const string&  memo );
+      using transfer_action = eosio::action_wrapper<"transfer"_n, &amax_token::transfer>;
+};
+
+#define TRANSFER_OUT(token_contract, to, quantity, memo)                             \
+            amax_token::transfer_action(token_contract, {{get_self(), ACTIVE_PERM}}) \
+               .send(get_self(), to, quantity, memo);
+
+
+namespace db {
+
+    template<typename table, typename Lambda>
+    inline void set(table &tbl,  typename table::const_iterator& itr, const eosio::name& emplaced_payer,
+            const eosio::name& modified_payer, Lambda&& setter )
+   {
+        if (itr == tbl.end()) {
+            tbl.emplace(emplaced_payer, [&]( auto& p ) {
+               setter(p, true);
+            });
+        } else {
+            tbl.modify(itr, modified_payer, [&]( auto& p ) {
+               setter(p, false);
+            });
+        }
+    }
+
+    template<typename table, typename Lambda>
+    inline void set(table &tbl,  typename table::const_iterator& itr, const eosio::name& emplaced_payer,
+               Lambda&& setter )
+   {
+      set(tbl, itr, emplaced_payer, eosio::same_payer, setter);
+   }
+
+}// namespace db
+
+void amax_reward::updatevotes(const name& voter_name, const std::set<name>& producers, double votes) {
+    require_auth( SYSTEM_CONTRACT );
+    require_auth( voter_name );
+    auto now = eosio::current_time_point();
+
+   amax_reward::voter::table voter_tbl(get_self(), get_self().value);
+   producer::table producer_tbl(get_self(), get_self().value);
+   std::map<name, vote_reward_info> new_producers;
+   int64_t new_reward_amount = 0;
+
+   auto voter_itr = voter_tbl.find(voter_name.value);
+   if (voter_itr != voter_tbl.end()) {
+      // old_votes = voter_itr->votes;
+      if (voter_itr->votes > 0) {
+         for ( const auto& voted_prod : voter_itr->producers) {
+            const auto& prod_name = voted_prod.first;
+            auto &last_reward_per_vote = voted_prod.second.last_reward_per_vote;
+            auto prod = producer_tbl.get(prod_name.value, "the voted producer not found");
+
+            producer_tbl.modify( prod, eosio::same_payer, [&]( auto& p ) {
+               // allocate rewards
+               if (last_reward_per_vote > p.reward_per_vote) {
+                  uint64_t amount = (last_reward_per_vote - p.reward_per_vote) * voter_itr->votes;
+                  check(p.allocating_rewards.amount >= amount, "producer allocating rewards insufficient");
+                  p.allocating_rewards.amount -= amount;
+                  p.allocated_rewards.amount += amount;
+                  new_reward_amount += amount;
+                  p.update_at = now;
+               }
+               p.votes += votes - voter_itr->votes;
+            });
+
+
+            if (producers.count(prod_name)) {
+               new_producers[prod_name].last_reward_per_vote =  prod.reward_per_vote;
+            }
+         }
+      }
+   }
+
+   for (const auto &prod_name : producers) {
+      if (new_producers.count(prod_name) == 0) {
+         auto prod_itr = producer_tbl.find(prod_name.value);
+         if (prod_itr != producer_tbl.end()) {
+            new_producers[prod_name].last_reward_per_vote = prod_itr->reward_per_vote;
+         }
+         db::set(producer_tbl, prod_itr, voter_name, [&]( auto& p, bool is_new ) {
+            p.owner = prod_name;
+            p.votes += votes;
+            p.update_at = now;
+         });
+      }
+   }
+
+   db::set(voter_tbl, voter_itr, voter_name, [&]( auto& v, bool is_new ) {
+      if (is_new) {
+         v.owner = voter_name;
+      } else {
+         v.unclaimed_rewards.amount += new_reward_amount;
+      }
+      v.producers = std::move(new_producers);
+      v.votes = votes;
+      v.update_at = now;
+   });
+
+}
+
+
+void amax_reward::addrewards(const name& producer_name, const asset& quantity) {
+   require_auth(producer_name);
+   check(is_account(producer_name), "producer account not found");
+   check(quantity.symbol == CORE_SYMBOL, "symbol mismatch");
+   check(quantity.amount > 0, "quanitty must be positive");
+   check(quantity <= _gstate.reward_balance, "reward balance insufficient");
+   auto now = eosio::current_time_point();
+
+   producer::table producer_tbl(get_self(), get_self().value);
+   auto prod_itr = producer_tbl.find(producer_name.value);
+   db::set(producer_tbl, prod_itr, producer_name, [&]( auto& p, bool is_new ) {
+      if (is_new) {
+         p.owner =  producer_name;
+      }
+
+      if (p.votes > 0) {
+         p.allocating_rewards += quantity;
+         p.reward_per_vote += quantity.amount / p.votes;
+      } else {
+         p.unallocated_rewards += quantity;
+      }
+
+      p.update_at = now;
+   });
+
+}
+
+void amax_reward::claimrewards(const name& voter_name) {
+
+   require_auth( voter_name );
+   auto now = eosio::current_time_point();
+   amax_reward::voter::table voter_tbl(get_self(), get_self().value);
+   int64_t new_reward_amount = 0;
+
+   auto voter_itr = voter_tbl.find(voter_name.value);
+   check(voter_itr != voter_tbl.end(), "voter info not found");
+   check(voter_itr->votes > 0, "votes not positive");
+
+   producer::table producer_tbl(get_self(), get_self().value);
+   for ( const auto& voted_prod : voter_itr->producers) {
+      const auto& prod_name = voted_prod.first;
+      auto &last_reward_per_vote = voted_prod.second.last_reward_per_vote;
+      auto prod = producer_tbl.get(prod_name.value, "the voted producer not found");
+
+      if (last_reward_per_vote > prod.reward_per_vote) {
+         uint64_t amount = (last_reward_per_vote - prod.reward_per_vote) * voter_itr->votes;
+         check(prod.allocating_rewards.amount >= amount, "producer allocating rewards insufficient");
+         new_reward_amount += amount;
+
+         producer_tbl.modify( prod, eosio::same_payer, [&]( auto& p ) {
+            p.allocating_rewards.amount -= amount;
+            p.allocated_rewards.amount += amount;
+            p.update_at = now;
+         });
+      }
+   }
+
+   auto unclaimed_rewards = voter_itr->unclaimed_rewards;
+   unclaimed_rewards.amount += new_reward_amount;
+   check(unclaimed_rewards.amount > 0, "no rewards to claim");
+
+   TRANSFER_OUT(CORE_TOKEN, voter_name, unclaimed_rewards, "voted rewards");
+
+   voter_tbl.modify(voter_itr, voter_name, [&]( auto& v) {
+      v.unclaimed_rewards.amount = 0;
+      v.claimed_rewards += unclaimed_rewards;
+      v.update_at = now;
+   });
+}
+
+void amax_reward::ontransfer(    const name &from,
+                                 const name &to,
+                                 const asset &quantity,
+                                 const string &memo)
+{
+   if (quantity.symbol == CORE_SYMBOL && from != get_self() && to == get_self()) {
+      _gstate.reward_balance += quantity;
+      _gstate.total_rewards += quantity;
+   }
+}
+
+} /// namespace eosio
