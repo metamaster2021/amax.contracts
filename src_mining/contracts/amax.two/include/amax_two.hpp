28e834afd4c2df8f41dd788be584ecf00ce76462
EdisonChenZX <1648458952@qq.com>
2023-03-17 11:39:16 +0800

    add amax.two contract

---
+++ src_mining/contracts/amax.two/include/amax_two.hpp
@@ -0,0 +1,60 @@
+#include "amax_two.db.hpp"
+#include <eosio/action.hpp>
+#include <wasm_db.hpp>
+
+
+using namespace std;
+using namespace wasm::db;
+
+class [[eosio::contract("amax.two")]] amax_two: public eosio::contract {
+private:
+    global_singleton    _global;
+    global_t            _gstate;
+
+public:
+    using contract::contract;
+
+    amax_two(eosio::name receiver, eosio::name code, datastream<const char*> ds):
+         contract(receiver, code, ds),
+        _global(get_self(), get_self().value)
+    {
+        _gstate = _global.exists() ? _global.get() : global_t{};
+    }
+    ~amax_two() { _global.set( _gstate, get_self() ); }
+
+
+    [[eosio::action]] void init(const name& admin, const name& mine_token_contract, time_point_sec started_at, time_point_sec ended_at);
+
+
+    /**
+     * ontransfer, trigger by recipient of transfer()
+     * @param memo - memo format:
+     * 1. ads_id:${ads_id}, pay plan fee, Eg: "ads_id:" or "ads_id:1"
+     *
+     *    transfer() params:
+     *    @param from - issuer
+     *    @param to   - must be contract self
+     *    @param quantity - issued quantity
+     */
+    [[eosio::on_notify("aplink.token::transfer")]] void ontransfer(name from, name to, asset quantity, string memo);
+
+    [[eosio::action]] void aplswaplog(
+                    const name&         miner,
+                    const asset&        recd_apls,
+                    const asset&        swap_tokens,
+                    const time_point&   created_at);
+    
+    using aplswaplog_action = eosio::action_wrapper<"aplswaplog"_n, &amax_two::aplswaplog>;
+
+private: 
+    void _claim_reward( const name& to, const asset& recd_apls, const string& memo );
+    void _cal_reward( asset& reward, const name& to, const asset& recd_apls );
+    void _on_apl_swap_log(
+                    const name&         miner,
+                    const asset&        recd_apls,
+                    const asset&        swap_tokens,
+                    const time_point&   created_at);
+
+
+    
+}; //contract amax.two
---
+++ src_mining/contracts/amax.two/src/amax_two.cpp
@@ -0,0 +1,82 @@
+
+#include <amax.token.hpp>
+#include "amax_two.hpp"
+#include "utils.hpp"
+#include <cmath>
+#include <chrono>
+#include <amax.token/amax.token.hpp>
+#include <aplink.token/aplink.token.hpp>
+
+
+using std::chrono::system_clock;
+using namespace wasm;
+
+static constexpr eosio::name active_permission{"active"_n};
+
+// transfer out from contract self
+#define TRANSFER_OUT(token_contract, to, quantity, memo) token::transfer_action(                                \
+                                                             token_contract, {{get_self(), active_permission}}) \
+                                                             .send(                                             \
+                                                                 get_self(), to, quantity, memo);
+
+void amax_two::init(const name& admin, const name& mine_token_contract, time_point_sec started_at, time_point_sec ended_at) {
+    require_auth( _self );
+    _gstate.admin                   = admin;
+    _gstate.mine_token_contract     = mine_token_contract;
+    _gstate.started_at              = started_at;
+    _gstate.ended_at                = ended_at;
+}
+
+void amax_two::ontransfer(name from, name to, asset quantity, string memo) {
+    if (from == get_self() || to != get_self()) return;
+
+	CHECK( quantity.amount > 0, "quantity must be positive" )
+
+    if(amax::token::is_blacklisted("amax.token"_n, from))
+        return;
+    CHECK( time_point_sec(current_time_point()) >= _gstate.started_at, "amax #2 not open yet" )
+    CHECK( time_point_sec(current_time_point()) <  _gstate.ended_at, "amax #2 already ended" )
+    CHECK( quantity.symbol == APL_SYMBOL, "Ntwo APL symbol not allowed: " + quantity.to_string() )
+
+    _claim_reward(from, quantity, "");
+}
+
+void amax_two::aplswaplog( const name& miner, const asset& recd_apls, const asset& swap_tokens, const time_point& created_at) {
+    require_auth(get_self());
+    require_recipient(miner);
+}
+
+void amax_two::_claim_reward( const name& to, 
+                                const asset& recd_apls,
+                                const string& memo )
+{
+
+    asset reward =asset(0, SYS_SYMBOL);
+    _cal_reward(reward, to, recd_apls);
+    CHECK( reward <= _gstate.mine_token_remained, "reward token not enough" )
+    _gstate.mine_token_remained = _gstate.mine_token_remained - reward;
+
+    TRANSFER(_gstate.mine_token_contract, to, reward, memo )
+    _on_apl_swap_log(to, recd_apls, reward, current_time_point());
+
+}
+
+void amax_two::_cal_reward( asset&   reward, 
+                            const name&   to,
+                            const asset&  recd_apls )
+{
+    asset sumbalance = aplink::token::get_sum( APL_CONTRACT, to, APL_SYMBOL.code() );  
+    double sbt =  sumbalance.amount/PERCENT_BOOST;
+    double a = 1 + power(log(sbt- 800)/16, 2);
+    int64_t amount = a * (recd_apls.amount / PERCENT_BOOST / 400) * AMAX_PRECISION;
+    reward.set_amount(amount);
+}
+
+void amax_two::_on_apl_swap_log(
+                    const name&         miner,
+                    const asset&        recd_apls,
+                    const asset&        swap_tokens,
+                    const time_point&   created_at) {
+    amax_two::aplswaplog_action act{ _self, { {_self, active_permission} } };
+    act.send( miner, recd_apls, swap_tokens, created_at );
+}
